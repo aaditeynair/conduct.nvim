74114d49f3b4f916f1da999cd71285498fdc58b8
aaditeynair <aaditeyn@gmail.com>
2023-04-17 13:12:42 +0530

    feat: create a function that creates new projects. link that to a vim command

--- lua/conduct/init.lua
+++ lua/conduct/init.lua
@@ -1,3 +1,6 @@
+local Path = require("plenary.path")
+local data_folder = vim.fn.stdpath("data") .. "/conduct/"
+
 local M = {}
 
 M.presets = {}
@@ -15,6 +18,31 @@ function M.setup(opts)
         M.presets = {}
         print("presets must be a table")
     end
+end
+
+function M.create_project(project_name)
+    local new_project = {
+        name = project_name,
+        cwd = vim.fn.getcwd(),
+        keybinds = {},
+        preset = "",
+    }
+
+    Path:new(data_folder):mkdir()
+
+    local project_file_location = data_folder .. "/" .. project_name .. ".json"
+    local project_file = Path:new(project_file_location)
+
+    if not project_file:exists() then
+        project_file:touch()
+    else
+        print("Overwriting existing project")
+    end
+
+    local data = vim.json.encode(new_project)
+    project_file:write(data, "w")
+
+    vim.cmd("e " .. project_file_location)
 end
 
 return M
--- plugin/conduct.lua
+++ plugin/conduct.lua
@@ -3,3 +3,7 @@ if vim.g.conduct_loaded then
 end
 
 vim.g.conduct_loaded = 1
+
+vim.api.nvim_create_user_command("ConductNewProject", function(opts)
+    require("conduct").create_project(opts.args)
+end, { nargs = 1 })
