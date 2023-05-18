9c9d1d0a777f471be16a898c34f3a384100982fc
aaditeynair <aaditeyn@gmail.com>
2023-05-18 11:04:15 +0530

    feat: add keymap for deleting project from telescope search

--- lua/conduct/telescope.lua
+++ lua/conduct/telescope.lua
@@ -19,12 +19,34 @@ function M.search_projects(opts)
                 results = all_projects,
             }),
             sorter = conf.generic_sorter(opts),
-            attach_mappings = function(prompt_bufnr, _)
+            attach_mappings = function(prompt_bufnr, map)
                 actions.select_default:replace(function()
                     actions.close(prompt_bufnr)
                     local selection = action_state.get_selected_entry()
                     require("conduct").load_project(selection[1])
                 end)
+
+                map({ "i", "n" }, "<C-d>", function()
+                    actions.close(prompt_bufnr)
+                    local selection = action_state.get_selected_entry()
+
+                    if selection == nil then
+                        print("no project selected")
+                        return
+                    end
+
+                    local project_name = selection[1]
+                    vim.ui.input({ prompt = "confirm project delete [y/n]: " }, function(confirm)
+                        if confirm ~= "y" then
+                            print("aborting...")
+                            return
+                        end
+
+                        print("deleting " .. project_name .. "...")
+                        require("conduct").delete_project(project_name)
+                    end)
+                end)
+
                 return true
             end,
         })
